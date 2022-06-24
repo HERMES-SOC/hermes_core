cf4dd2544a507fba82851d4b28b9c4359372ee57
Steven Daniel Christe <steven.d.christe@nasa.gov>
2022-06-24 14:28:55 -0400

    added test for output and more error checks

--- hermes_core/tests/test_util_util.py
+++ hermes_core/tests/test_util_util.py
@@ -1,6 +1,56 @@
 """Tests for util.py"""
-from hermes_core.util import util
 import pytest
+
+from astropy.time import Time
+from hermes_core.util import util
+
+
+def test_science_filename_output():
+    """Test expected output"""
+    time = "2024-04-06T12:06:21"
+    time_formatted = "20240406_120621"
+
+    assert util.create_science_filename(
+        "eea", time, level="l0", version="1.2.3"
+    ) == "hermes_eea_l0_{}_v1.2.3".format(time_formatted)
+    # merit
+    assert util.create_science_filename(
+        "merit", time, level="l2", version="2.4.5"
+    ) == "hermes_mrt_l2_{}_v2.4.5".format(time_formatted)
+    # nemisis and version
+    assert util.create_science_filename(
+        "nemisis", time, level="l2", version="1.3.5"
+    ) == "hermes_nms_l2_{}_v1.3.5".format(time_formatted)
+    # spani and level
+    assert util.create_science_filename(
+        "spani", time, level="l3", version="2.4.5"
+    ) == "hermes_spn_l3_{}_v2.4.5".format(time_formatted)
+    # mode
+    assert util.create_science_filename(
+        "spani", time, level="l3", mode="2s", version="2.4.5"
+    ) == "hermes_spn_2s_l3_{}_v2.4.5".format(time_formatted)
+    # test
+    assert util.create_science_filename(
+        "spani", time, level="l0", version="2.4.5", test=True
+    ) == "hermes_spn_l0test_{}_v2.4.5".format(time_formatted)
+    # all options
+    assert util.create_science_filename(
+        "spani", time, level="l3", mode="2s", descriptor="burst", version="2.4.5", test=True
+    ) == "hermes_spn_2s_l3test_burst_{}_v2.4.5".format(time_formatted)
+    # Time object instead of str
+    assert util.create_science_filename(
+        "spani", Time(time), level="l3", mode="2s", descriptor="burst", version="2.4.5", test=True
+    ) == "hermes_spn_2s_l3test_burst_{}_v2.4.5".format(time_formatted)
+    # Time object but created differently
+    assert util.create_science_filename(
+        "spani",
+        Time(2460407.004409722, format="jd"),
+        level="l3",
+        mode="2s",
+        descriptor="burst",
+        version="2.4.5",
+        test=True,
+    ) == "hermes_spn_2s_l3test_burst_{}_v2.4.5".format(time_formatted)
 
 
 def test_science_filename_exceptions():
@@ -16,10 +66,23 @@ def test_science_filename_exceptions():
 
         # wrong level specification
         util.create_science_filename(good_instrument, good_time, level="la", version=good_version)
-        util.create_science_filename(good_instrument, good_time, level="squirrel", version=good_version)
+        util.create_science_filename(
+            good_instrument, good_time, level="squirrel", version=good_version
+        )
 
         # wrong instrument name
         util.create_science_filename("eeb", good_time, level=good_level, version=good_version)
         util.create_science_filename("fpi", good_time, level=good_level, version=good_version)
         util.create_science_filename("potato", good_time, level=good_level, version=good_version)
 
+        # bad time string
+        # non-existent time
+        util.create_science_filename(
+            "eeb", "2023-13-04T12:06:21", level=good_level, version=good_version
+        )
+        # not isot format
+        util.create_science_filename(
+            "eeb", "2023/13/04 12:06:21", level=good_level, version=good_version
+        )
+        # not valid input for time
+        util.create_science_filename("eeb", time=12345345, level=good_level, version=good_version)
