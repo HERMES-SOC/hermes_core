4e4a029ce5c1905803d1a572e4e6fcd139e96457
Steven Christe <ehsteve@users.noreply.github.com>
2022-09-27 12:09:45 -0400

    Added support for parsing level 0 files (#25)

--- hermes_core/tests/test_util_util.py
+++ hermes_core/tests/test_util_util.py
@@ -4,35 +4,40 @@ import pytest
 from astropy.time import Time
 from hermes_core.util import util
 
+time = "2024-04-06T12:06:21"
+time_formatted = "20240406_120621"
 
-def test_science_filename_output():
-    """Test expected output"""
-    time = "2024-04-06T12:06:21"
-    time_formatted = "20240406_120621"
 
-    assert util.create_science_filename(
-        "eea", time, level="l0", version="1.2.3"
-    ) == "hermes_eea_l0_{}_v1.2.3".format(time_formatted)
-    # merit
-    assert util.create_science_filename(
-        "merit", time, level="l2", version="2.4.5"
-    ) == "hermes_mrt_l2_{}_v2.4.5".format(time_formatted)
-    # nemisis and version
-    assert util.create_science_filename(
-        "nemisis", time, level="l2", version="1.3.5"
-    ) == "hermes_nms_l2_{}_v1.3.5".format(time_formatted)
-    # spani and level
-    assert util.create_science_filename(
-        "spani", time, level="l3", version="2.4.5"
-    ) == "hermes_spn_l3_{}_v2.4.5".format(time_formatted)
+# fmt: off
+@pytest.mark.parametrize(
+    "instrument, time, level, version, result",
+    [
+        ("eea", time, "l1", "1.2.3", "hermes_eea_l1_{}_v1.2.3.cdf".format(time_formatted)),
+        ("merit", time, "l2", "2.4.5", "hermes_mrt_l2_{}_v2.4.5.cdf".format(time_formatted)),
+        ("nemisis", time, "l2", "1.3.5", "hermes_nms_l2_{}_v1.3.5.cdf".format(time_formatted)),
+        ("spani", time, "l3", "2.4.5", "hermes_spn_l3_{}_v2.4.5.cdf".format(time_formatted)),
+    ],
+)
+def test_science_filename_output_a(instrument, time, level, version, result):
+    """Test simple cases with expected output"""
+    assert (
+        util.create_science_filename(instrument, time, level=level, version=version)
+        == result
+    )
+# fmt: on
+
+
+def test_science_filename_output_b():
+    """Test more complex cases of expected output"""
+
     # mode
     assert util.create_science_filename(
         "spani", time, level="l3", mode="2s", version="2.4.5"
-    ) == "hermes_spn_2s_l3_{}_v2.4.5".format(time_formatted)
+    ) == "hermes_spn_2s_l3_{}_v2.4.5.cdf".format(time_formatted)
     # test
     assert util.create_science_filename(
-        "spani", time, level="l0", version="2.4.5", test=True
-    ) == "hermes_spn_l0test_{}_v2.4.5".format(time_formatted)
+        "spani", time, level="l1", version="2.4.5", test=True
+    ) == "hermes_spn_l1test_{}_v2.4.5.cdf".format(time_formatted)
     # all options
     assert util.create_science_filename(
         "spani",
@@ -42,7 +47,7 @@ def test_science_filename_output():
         descriptor="burst",
         version="2.4.5",
         test=True,
-    ) == "hermes_spn_2s_l3test_burst_{}_v2.4.5".format(time_formatted)
+    ) == "hermes_spn_2s_l3test_burst_{}_v2.4.5.cdf".format(time_formatted)
     # Time object instead of str
     assert util.create_science_filename(
         "spani",
@@ -52,7 +57,7 @@ def test_science_filename_output():
         descriptor="burst",
         version="2.4.5",
         test=True,
-    ) == "hermes_spn_2s_l3test_burst_{}_v2.4.5".format(time_formatted)
+    ) == "hermes_spn_2s_l3test_burst_{}_v2.4.5.cdf".format(time_formatted)
     # Time object but created differently
     assert util.create_science_filename(
         "spani",
@@ -62,81 +67,7 @@ def test_science_filename_output():
         descriptor="burst",
         version="2.4.5",
         test=True,
-    ) == "hermes_spn_2s_l3test_burst_{}_v2.4.5".format(time_formatted)
-
-
-def test_science_filename_exceptions():
-    """Test for errors"""
-    good_time = "2025-06-02T12:04:01"
-    good_instrument = "eea"
-    good_level = "l0"
-    good_version = "1.3.4"
-    with pytest.raises(ValueError):
-        # not enough depth to version number
-        util.create_science_filename(
-            good_instrument, good_time, level=good_level, version="1.3"
-        )
-        util.create_science_filename(
-            good_instrument, good_time, level=good_level, version="1"
-        )
-        util.create_science_filename(
-            good_instrument, good_time, level=good_level, version="1.5.6.7"
-        )
-        util.create_science_filename(
-            good_instrument, good_time, level=good_level, version="1.."
-        )
-        # a letter in version number
-        util.create_science_filename(
-            good_instrument, good_time, level=good_level, version="a.5.6"
-        )
-
-        # wrong level specification
-        util.create_science_filename(
-            good_instrument, good_time, level="la", version=good_version
-        )
-        util.create_science_filename(
-            good_instrument, good_time, level="squirrel", version=good_version
-        )
-
-        # wrong instrument name
-        util.create_science_filename(
-            "eeb", good_time, level=good_level, version=good_version
-        )
-        util.create_science_filename(
-            "fpi", good_time, level=good_level, version=good_version
-        )
-        util.create_science_filename(
-            "potato", good_time, level=good_level, version=good_version
-        )
-
-        # bad time string
-        # non-existent time
-        util.create_science_filename(
-            good_instrument,
-            "2023-13-04T12:06:21",
-            level=good_level,
-            version=good_version,
-        )
-        # not isot format
-        util.create_science_filename(
-            "eeb", "2023/13/04 12:06:21", level=good_level, version=good_version
-        )
-        # not valid input for time
-        util.create_science_filename(
-            "eeb", time=12345345, level=good_level, version=good_version
-        )
-        # _ character in mode
-        util.create_science_filename(
-            "eeb", time=12345345, level=good_level, version=good_version, mode="o_o"
-        )
-        # _ character in descriptor
-        util.create_science_filename(
-            "eeb",
-            time=12345345,
-            level=good_level,
-            version=good_version,
-            descriptor="blue_green",
-        )
+    ) == "hermes_spn_2s_l3test_burst_{}_v2.4.5.cdf".format(time_formatted)
 
 
 def test_parse_science_filename_output():
@@ -224,8 +155,8 @@ def test_parse_science_filename_output():
     assert util.parse_science_filename(f) == input
 
 
-def test_parse_science_filename_errors():
-    """Test for errors"""
+def test_parse_science_filename_errors_l1():
+    """Test for errors in l1 and above files"""
     with pytest.raises(ValueError):
         # wrong mission name
         f = "veeger_spn_2s_l3test_burst_20240406_120621_v2.4.5"
@@ -234,3 +165,73 @@ def test_parse_science_filename_errors():
         # wrong instrument name
         f = "hermes_www_2s_l3test_burst_20240406_120621_v2.4.5"
         util.parse_science_filename(f)
+
+
+good_time = "2025-06-02T12:04:01"
+good_instrument = "eea"
+good_level = "l1"
+good_version = "1.3.4"
+
+
+# fmt: off
+@pytest.mark.parametrize(
+    "instrument,time,level,version",
+    [
+        (good_instrument, good_time, good_level, "1.3"),  # bad version specifications
+        (good_instrument, good_time, good_level, "1"),
+        (good_instrument, good_time, good_level, "1.5.6.7"),
+        (good_instrument, good_time, good_level, "1.."),
+        (good_instrument, good_time, good_level, "a.5.6"),
+        (good_instrument, good_time, "la", good_version),  # wrong level specifications
+        (good_instrument, good_time, "squirrel", good_version),
+        (good_instrument, good_time, "l0", good_version),
+        ("potato", good_time, good_level, good_version),  # wrong instrument names
+        ("eeb", good_time, good_level, good_version),
+        ("fpi", good_time, good_level, good_version),
+        (good_instrument, "2023-13-04T12:06:21", good_level, good_version),  # non-existent time
+        (good_instrument, "2023/13/04 12:06:21", good_level, good_version),  # not isot format
+        (good_instrument, "2023/13/04 12:06:21", good_level, good_version),  # not isot format
+        (good_instrument, "12345345", good_level, good_version),  # not valid input for time
+    ]
+)
+def test_science_filename_errors_l1_a(instrument, time, level, version):
+    """"""
+    with pytest.raises(ValueError) as e:
+        util.create_science_filename(
+            instrument, time, level=level, version=version
+        )
+# fmt: on
+
+
+def test_science_filename_errors_l1_b():
+    with pytest.raises(ValueError):
+        # _ character in mode
+        util.create_science_filename(
+            "eeb", time="12345345", level=good_level, version=good_version, mode="o_o"
+        )
+    with pytest.raises(ValueError):
+        # _ character in descriptor
+        util.create_science_filename(
+            "eeb",
+            time="12345345",
+            level=good_level,
+            version=good_version,
+            descriptor="blue_green",
+        )
+
+
+# fmt: off
+@pytest.mark.parametrize("filename,instrument,time,level,version", [
+    ("hermes_MAG_l0_2024094-124603_v01.bin", "nemisis", "2024-04-03T12:46:03", "l0", "01"),
+    ("hermes_EEA_l0_2026337-124603_v11.bin", "eea", "2026-12-03T12:46:03", "l0", "11"),
+    ("hermes_MERIT_l0_2026215-124603_v21.bin", "merit", "2026-08-03T12:46:03", "l0", "21"),
+    ("hermes_SPANI_l0_2026337-065422_v11.bin", "spani", "2026-12-03T06:54:22", "l0", "11"),
+])
+def test_parse_l0_filenames(filename, instrument, time, level, version):
+    """Testing parsing of MOC-generated level 0 files."""
+    result = util.parse_science_filename(filename)
+    assert result['instrument'] == instrument
+    assert result['level'] == level
+    assert result['version'] == version
+    assert result['time'] == Time(time)
+# fmt: on
