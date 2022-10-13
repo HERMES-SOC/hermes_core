21de631faf0230a896ea407590831672db43b008
Steven Christe <ehsteve@users.noreply.github.com>
2022-10-13 13:07:34 -0400

    log message on import

--- hermes_core/__init__.py
+++ hermes_core/__init__.py
@@ -23,3 +23,5 @@ INST_SHORTNAMES = ["eea", "nms", "mrt", "spn"]
 INST_TARGETNAMES = ["EEA", "MAG", "MERIT", "SPANI"]
 INST_TO_SHORTNAME = dict(zip(INST_NAMES, INST_SHORTNAMES))
 INST_TO_TARGETNAME = dict(zip(INST_NAMES, INST_TARGETNAMES))
+
+log.info(f"hermes_core version: {__version__}")
