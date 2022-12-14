e423964564da580746854d3687e728618602242b
Steven Christe <steven.d.christe@nasa.gov>
2022-12-14 12:41:46 -0600

    doc test skips

--- hermes_core/io/file_tools.py
+++ hermes_core/io/file_tools.py
@@ -26,18 +26,17 @@ def read_file(data_filename: str) -> TimeSeries:
     Examples
     --------
     >>> from hermes_core.io import read_file
-    >>> data = read_file('hermes_sunsensor_l1_20220501_000000_v1.0.0.cdf')
+    >>> data = read_file('hermes_sunsensor_l1_20220501_000000_v1.0.0.cdf')  # doctest: +SKIP
     """
 
     file_metadata = parse_science_filename(data_filename)
-    if file_metadata['level'] == 'l0':
+    if file_metadata["level"] == "l0":
         raise ValueError("This function does not support level 0 files.")
 
-    # open the file and extract the data and put it into a TimeSeries if possible
-
+    # open the file and extract the data and put it into a TimeSeries (see below for example)
 
     # example of a data structure
-    data = TimeSeries(time_start='2016-03-22T12:30:31', time_delta=3 * u.s, n_samples=5)
-    data['electron flux'] = range(5) * u.electron / (u.s * u.cm ** 2 * u.sr)
+    data = TimeSeries(time_start="2016-03-22T12:30:31", time_delta=3 * u.s, n_samples=5)
+    data["electron flux"] = range(5) * u.electron / (u.s * u.cm**2 * u.sr)
 
     return data
