f92bdc19a227391816c3f7f7bc77ce0617091296
Steven Christe <steven.d.christe@nasa.gov>
2023-06-17 11:06:44 -0400

    added full instrument names

--- hermes_core/__init__.py
+++ hermes_core/__init__.py
@@ -20,8 +20,13 @@ __all__ = ["config", "print_config"]
 MISSION_NAME = "hermes"
 INST_NAMES = ["eea", "nemisis", "merit", "spani"]
 INST_SHORTNAMES = ["eea", "nms", "mrt", "spn"]
+INST_FULLNAMES = ["Electron Electrostatic Analyzer",
+                  "Noise Eliminating Magnetometer Instrument in a Small Integrated System",
+                  "Miniaturized Electron pRoton Telescope",
+                  "Solar Probe Analyzer for Ions"]
 INST_TARGETNAMES = ["EEA", "MAG", "MERIT", "SPANI"]
 INST_TO_SHORTNAME = dict(zip(INST_NAMES, INST_SHORTNAMES))
 INST_TO_TARGETNAME = dict(zip(INST_NAMES, INST_TARGETNAMES))
+INST_TO_FULLNAME = dict(zip(INST_NAMES, INST_FULLNAMES))
 
 # log.info(f"hermes_core version: {__version__}")
