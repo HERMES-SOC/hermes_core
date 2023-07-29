250c28fa3d7a9c67f2610f9e000c69545a9d191c
Steven Christe <ehsteve@users.noreply.github.com>
2023-07-29 17:30:59 -0400

    added copy button to code blocks

--- docs/conf.py
+++ docs/conf.py
@@ -38,6 +38,7 @@ extensions = [
     "matplotlib.sphinxext.plot_directive",
     "sphinx_automodapi.automodapi",
     "sphinx_automodapi.smart_resolver",
+    "sphinx_copybutton",
 ]
 
 # Add any paths that contain templates here, relative to this directory.
