135a31cce2fb0ce45138ec24f122bcab468d257a
Andrew Robbertz <24920994+Alrobbertz@users.noreply.github.com>
2024-07-24 09:03:00 -0400

    Initial Integration

--- hermes_core/__init__.py
+++ hermes_core/__init__.py
@@ -7,29 +7,17 @@ except ImportError:
     version_tuple = (0, 0, "unknown version")
 
 from hermes_core.util.config import load_config, print_config
-from hermes_core.util.logger import _init_log
+from swxsoc.util.logger import _init_log
+import swxsoc
 
 # Load user configuration
 config = load_config()
+# Overwrite SWxSOC Config with HERMES Config
+swxsoc.config = config
 
 log = _init_log(config=config)
 
 # Then you can be explicit to control what ends up in the namespace,
 __all__ = ["config", "print_config"]
-
-MISSION_NAME = "hermes"
-INST_NAMES = ["eea", "nemisis", "merit", "spani"]
-INST_SHORTNAMES = ["eea", "nem", "mrt", "spn"]
-INST_FULLNAMES = [
-    "Electron Electrostatic Analyzer",
-    "Noise Eliminating Magnetometer Instrument in a Small Integrated System",
-    "Miniaturized Electron pRoton Telescope",
-    "Solar Probe Analyzer for Ions",
-]
-INST_TARGETNAMES = ["EEA", "NEM", "MERIT", "SPANI"]
-INST_TO_SHORTNAME = dict(zip(INST_NAMES, INST_SHORTNAMES))
-INST_TO_TARGETNAME = dict(zip(INST_NAMES, INST_TARGETNAMES))
-INST_TO_FULLNAME = dict(zip(INST_NAMES, INST_FULLNAMES))
-DEFAULT_TIMESERIES_KEY = "Epoch"
 
 # log.info(f"hermes_core version: {__version__}")
