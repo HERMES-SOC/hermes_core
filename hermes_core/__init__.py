009a0d122199a03060c81d4a7b2dda40358e63d3
Andrew Robbertz <24920994+Alrobbertz@users.noreply.github.com>
2024-09-26 11:28:14 -0400

    Generic SWxSOC Integration (#122)
    * Initial Integration
    
    * Update to use new SWxSOC Config
    
    * Remove Unused Module Variables
    
    * Update Customization

--- hermes_core/__init__.py
+++ hermes_core/__init__.py
@@ -6,30 +6,19 @@ except ImportError:
     __version__ = "unknown version"
     version_tuple = (0, 0, "unknown version")
 
-from hermes_core.util.config import load_config, print_config
-from hermes_core.util.logger import _init_log
+import os
+import swxsoc
 
-# Load user configuration
-config = load_config()
+# Set the Mission Environment Variable to load the correct configuration
+os.environ["SWXSOC_MISSION"] = "hermes"
+swxsoc._reconfigure()
+# Get the Updated Configuration
+config = swxsoc.config
 
-log = _init_log(config=config)
+# Initialize HERMES Logger
+log = swxsoc.util.logger._init_log(config)
 
 # Then you can be explicit to control what ends up in the namespace,
-__all__ = ["config", "print_config"]
-
-MISSION_NAME = "hermes"
-INST_NAMES = ["eea", "nemisis", "merit", "spani"]
-INST_SHORTNAMES = ["eea", "nem", "mrt", "spn"]
-INST_FULLNAMES = [
-    "Electron Electrostatic Analyzer",
-    "Noise Eliminating Magnetometer Instrument in a Small Integrated System",
-    "Miniaturized Electron pRoton Telescope",
-    "Solar Probe Analyzer for Ions",
-]
-INST_TARGETNAMES = ["EEA", "NEM", "MERIT", "SPANI"]
-INST_TO_SHORTNAME = dict(zip(INST_NAMES, INST_SHORTNAMES))
-INST_TO_TARGETNAME = dict(zip(INST_NAMES, INST_TARGETNAMES))
-INST_TO_FULLNAME = dict(zip(INST_NAMES, INST_FULLNAMES))
-DEFAULT_TIMESERIES_KEY = "Epoch"
+__all__ = ["config"]
 
 # log.info(f"hermes_core version: {__version__}")
--- hermes_core/util/util.py
+++ hermes_core/util/util.py
@@ -2,19 +2,10 @@
 This module provides general utility functions.
 """
 
-import os
-
-from astropy.time import Time
-
-import hermes_core
+from swxsoc.util import util
 
 
-__all__ = ["create_science_filename", "parse_science_filename", "VALID_DATA_LEVELS"]
-
-TIME_FORMAT_L0 = "%Y%j-%H%M%S"
-TIME_FORMAT = "%Y%m%dT%H%M%S"
-VALID_DATA_LEVELS = ["l0", "l1", "ql", "l2", "l3", "l4"]
-FILENAME_EXTENSION = ".cdf"
+__all__ = ["create_science_filename", "parse_science_filename"]
 
 
 def create_science_filename(
@@ -61,46 +52,9 @@ def create_science_filename(
     ValueError: If the data version does not match the HERMES data version formatting conventions
     ValueError: If the data product descriptor or instrument mode do not match the HERMES formatting conventions
     """
-    test_str = ""
-
-    if isinstance(time, str):
-        time_str = Time(time, format="isot").strftime(TIME_FORMAT)
-    else:
-        time_str = time.strftime(TIME_FORMAT)
-
-    if instrument not in hermes_core.INST_NAMES:
-        raise ValueError(
-            f"Instrument, {instrument}, is not recognized. Must be one of {hermes_core.INST_NAMES}."
-        )
-    if level not in VALID_DATA_LEVELS[1:]:
-        raise ValueError(
-            f"Level, {level}, is not recognized. Must be one of {VALID_DATA_LEVELS[1:]}."
-        )
-    # check that version is in the right format with three parts
-    if len(version.split(".")) != 3:
-        raise ValueError(
-            f"Version, {version}, is not formatted correctly. Should be X.Y.Z"
-        )
-    # check that version has integers in each part
-    for item in version.split("."):
-        try:
-            int_value = int(item)
-        except ValueError:
-            raise ValueError(f"Version, {version}, is not all integers.")
-
-    if test is True:
-        test_str = "test"
-
-    # the parse_science_filename function depends on _ not being present elsewhere
-    if ("_" in mode) or ("_" in descriptor):
-        raise ValueError(
-            "The underscore symbol _ is not allowed in mode or descriptor."
-        )
-
-    filename = f"hermes_{hermes_core.INST_TO_SHORTNAME[instrument]}_{mode}_{level}{test_str}_{descriptor}_{time_str}_v{version}"
-    filename = filename.replace("__", "_")  # reformat if mode or descriptor not given
-
-    return filename + FILENAME_EXTENSION
+    return util.create_science_filename(
+        instrument, time, level, version, mode, descriptor, test
+    )
 
 
 def parse_science_filename(filepath: str) -> dict:
@@ -124,77 +78,4 @@ def parse_science_filename(filepath: str) -> dict:
     ValueError: If the data level >0 for packet files
     ValueError: If not a CDF File
     """
-
-    result = {
-        "instrument": None,
-        "mode": None,
-        "test": False,
-        "time": None,
-        "level": None,
-        "version": None,
-        "descriptor": None,
-    }
-
-    filename = os.path.basename(filepath)
-    file_name, file_ext = os.path.splitext(filename)
-
-    filename_components = file_name.split("_")
-
-    if filename_components[0] != hermes_core.MISSION_NAME:
-        raise ValueError(f"File {filename} not recognized. Not a valid mission name.")
-
-    if file_ext == ".bin":
-        if filename_components[1] not in hermes_core.INST_TARGETNAMES:
-            raise ValueError(
-                f"File {filename} not recognized. Not a valid target name."
-            )
-
-        offset = 1 if len(filename_components) > 5 else 0
-
-        if offset:
-            result["mode"] = filename_components[2]
-
-        if filename_components[2 + offset] != VALID_DATA_LEVELS[0]:
-            raise ValueError(
-                f"Data level {filename_components[2 + offset]} is not correct for this file extension."
-            )
-        else:
-            result["level"] = filename_components[2 + offset]
-        #  reverse the dictionary to look up instrument name from the short name
-        from_shortname = {v: k for k, v in hermes_core.INST_TO_TARGETNAME.items()}
-
-        result["time"] = Time.strptime(filename_components[3 + offset], TIME_FORMAT_L0)
-
-    elif file_ext == ".cdf":
-        if filename_components[1] not in hermes_core.INST_SHORTNAMES:
-            raise ValueError(
-                "File {filename} not recognized. Not a valid instrument name."
-            )
-
-        #  reverse the dictionary to look up instrument name from the short name
-        from_shortname = {v: k for k, v in hermes_core.INST_TO_SHORTNAME.items()}
-
-        result["time"] = Time.strptime(filename_components[-2], TIME_FORMAT)
-
-        # mode and descriptor are optional so need to figure out if one or both or none is included
-        if filename_components[2][0:2] not in VALID_DATA_LEVELS:
-            # if the first component is not data level then it is mode and the following is data level
-            result["mode"] = filename_components[2]
-            result["level"] = filename_components[3].replace("test", "")
-            if "test" in filename_components[3]:
-                result["test"] = True
-            if len(filename_components) == 7:
-                result["descriptor"] = filename_components[4]
-        else:
-            result["level"] = filename_components[2].replace("test", "")
-            if "test" in filename_components[2]:
-                result["test"] = True
-            if len(filename_components) == 6:
-                result["descriptor"] = filename_components[3]
-    else:
-        raise ValueError(f"File extension {file_ext} not recognized.")
-
-    result["instrument"] = from_shortname[filename_components[1]]
-    result["version"] = filename_components[-1][1:]  # remove the v
-
-    return result
+    return util.parse_science_filename(filepath)
